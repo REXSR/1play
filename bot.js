1e6301fa95e59f031bb0324d7e1fce6d403db1ac
REXSR <41483292+REXSR@users.noreply.github.com>
2018-09-11 01:12:42 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -357,9 +357,47 @@ client.on('ready',async () => {
 
 
   
+@here , جميع الحقوق محفوظه بسيرفر كودز
+
+   =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- 
+
+  
+
+  client.on('guildCreate', guild => {
+
+client.channels.get("ROOM ID").send(`:white_check_mark: **${client.user.tag} دخل سيرفر جديد
+
+Server name: __${guild.name}__
+
+Server owner: __${guild.owner}__
+
+Server id: __${guild.id}__ 
+
+Server Count: __${guild.memberCount}__**`)
+
+});
+
+client.on('guildDelete', guild => {
+
+  client.channels.get("488406701582843920").send(`:negative_squared_cross_mark: **${client.user.tag} طلع من سيرفر
+
+Server name: __${guild.name}__
+
+Server owner: __${guild.owner}__
+
+Server id: __${guild.id}__ 
+
+Server Count: __${guild.memberCount}__**`)
+
+});
+
+  
 
  
 
+ 
+
+  
     
 
 
